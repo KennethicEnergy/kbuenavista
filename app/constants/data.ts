0ba30616e72a17dba63b5499442bee5c26645f5a
ken <kennethbuenavista.ccs@gmail.com>
2024-12-07 08:22:24 +0800

    docs: Updated readme

--- app/constants/data.ts
+++ app/constants/data.ts
@@ -32,6 +32,7 @@ export const timelineData = [
 		title: "Senior Front-End Web Developer",
 		company: "Collabera Digital",
 		companyUrl: "https://www.collaberadigital.com",
+    projectGalleryUrl: "https://www.collaberadigital.com",
 		date: "July 2022 - July 2023",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
