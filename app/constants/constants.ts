2e7e5a77420ee967c538a516eabd0c5461d0b1d6
ken <kennethbuenavista.ccs@gmail.com>
2026-01-31 13:25:13 +0800

    feat: add about page slideshow, use Link for faster nav, clear loader on route change

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -4,3 +4,13 @@ export const linkedinUrl = "https://www.linkedin.com/in/ken-buenavista-94a736144
 export const fullName = "Kenneth Buenavista"
 export const introduction = "I create beautiful websites your users will love."
 export const googleDocId = "1Hhe7097d9GVNDvz5JibYl1N80DdSg7wYhvK3CIbjjr0"
+
+/** About page slideshow. Use direct image URLs only (not post page links).
+ *  To get Instagram image URL: open the post on Instagram in a browser → right‑click the image → "Copy image address".
+ *  Or save images to public/images/ and use paths like "/images/instagram/1.jpg". */
+export const aboutSlideshowImages: string[] = [
+  'https://scontent.fcrk3-4.fna.fbcdn.net/v/t39.30808-6/492739483_29593092806955976_9028663801025713076_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=cbdqRlTEZHcQ7kNvwF2Z250&_nc_oc=Adm1UAZVnjQwG8_VbB3jn3W4M1OAC19aJGxwfvxekpri7oPahzQOGb1iGGAQwpfisss&_nc_zt=23&_nc_ht=scontent.fcrk3-4.fna&_nc_gid=paL-AmxBEzweu9Ydya4A8g&oh=00_AfsHw0mYTrkK3IicHRCIJHdiYQ8FWEReOrIMXoWoT-9vHQ&oe=69835C1D',
+  'https://scontent.fcrk3-4.fna.fbcdn.net/v/t1.6435-9/66153171_3051470804878204_1933181955503816704_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=f727a1&_nc_ohc=EGwiilTOUYwQ7kNvwGx_6X6&_nc_oc=AdlNYOMZ82Jy-_KqDXNiBthxYwoXOz_0bbbQTTEcZN1-OZH0gwViajQCbPC38KHpRK8&_nc_zt=23&_nc_ht=scontent.fcrk3-4.fna&_nc_gid=BMoI0i6ZgQT7_IOqQ46nsg&oh=00_AftTT9ELmvre0Hoyuj5Pi3feuVjduKH5X8B07PrcAGXq9Q&oe=69A50079',
+  'https://scontent.fcrk3-2.fna.fbcdn.net/v/t1.6435-9/65895298_3051471714878113_7353779919731556352_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=ZVEjkiubUGgQ7kNvwEBc06p&_nc_oc=Adl1xN8vqiiQfbx3bla4ZuW2_Gre3oAJc4I85F9P1lYwGXg4kju0CTEN1I-VRpR-aSA&_nc_zt=23&_nc_ht=scontent.fcrk3-2.fna&_nc_gid=Q8gCJYoUN5xlZfDyQSUoDg&oh=00_AfvlMWAa3FJxXY00DXNO9VwYXqtG-lXEJBrJG4JEsv0n_Q&oe=69A51AC0',
+  'https://scontent.fcrk3-3.fna.fbcdn.net/v/t1.6435-9/66197956_10156924052742779_3852457356658999296_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=V3lIZSjoTZgQ7kNvwGynbu4&_nc_oc=Adm04FmLXE2wfsS2LnqedVZtjXTiakG9CwXuTnML4RbO8lScF9VbHNIKuM_DMDYe5mE&_nc_zt=23&_nc_ht=scontent.fcrk3-3.fna&_nc_gid=Tfz0fCTa5TbXq-B6fqRiKg&oh=00_AfvxwDJvvJBrMytLA9UDLsxLkyYRIppqgBie3oOXcLrI2Q&oe=69A51C34',
+]
